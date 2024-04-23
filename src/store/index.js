05e05d679fdbfbcac2fae2de2f6ea09bfeb336b7
vitorbordalo <151081737+vitorbordalo@users.noreply.github.com>
2024-04-23 10:23:14 -0300

    shoppingbag

---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { createStore } from 'vuex'
+import axios from 'axios'
+
+export default createStore({
+  state: {
+    products: [],
+    productsInBag: []
+  },
+  mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+    },
+    removeFromBag(state, productId) {
+      var updatedBag = state.productsInBag.filter(item => productId != item.id);
+      state.productsInBag = updatedBag;
+    },
+  },
+  actions: {
+
+    loadProducts({ commit }) {
+      axios
+      .get('https://fakestoreapi.com/products')
+      .then(response => {
+        commit('loadProducts', response.data);
+      })
+    },
+
+    addToBag({ commit }, product) {
+      commit ('addToBag', product);
+    },
+    removeFromBag({ commit }, productId) {
+      commit ('removeFromBag', productId);
+    },
+
+  },
+  modules: {
+  }
+})
