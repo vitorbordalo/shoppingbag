af877f547289e4934b37f03cc66294d8902e4e6f
vitorbordalo <151081737+vitorbordalo@users.noreply.github.com>
2024-04-24 00:52:56 -0300

    shopping bag functionalities update

--- src/views/ShoppingBasket.vue
+++ src/views/ShoppingBasket.vue
@@ -1,112 +1,129 @@
 <template>
-    <div class="basket">
-      <div class="items">
-  
-        <div class="item">
-          <div class="remove">Remove item</div>
-          <div class="photo"><img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt=""></div>
-          <div class="description">Mens Casual Premium Slim Fit T-Shirts </div>
+  <div class="basket">
+    <div class="items">
+     
+     
+      <template v-if="productsInBag.length">
+     
+     
+        <div
+          v-for="(product, index) in productsInBag"
+          :key="index"
+          class="item"
+        >
+          <div class="remove" @click="this.$store.dispatch('removeFromBag', product.id)">Remove item</div>
+          <div class="photo">
+            <img :src="product.image" alt="" />
+          </div>
+          <div class="description">
+            {{ product.title }}
+          </div>
           <div class="price">
             <span class="quantity-area">
-              <button disabled="">-</button>
-              <span class="quantity">1</span>
-              <button>+</button>
+              <button
+                :disabled="product.quantity <= 1"
+                @click="product.quantity--"
+              >
+                -
+              </button>
+              <span class="quantity">{{ product.quantity }}</span>
+              <button @click="product.quantity++">+</button>
             </span>
-            <span class="amount">US$ 22.30</span>
+            <span class="amount"
+              >US$ {{ (product.price * product.quantity).toFixed(2) }}</span
+            >
           </div>
         </div>
-        <div class="grand-total"> Grand Total: US$ 22.30</div>
-  
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  
-  export default {
-    name: 'ShoppingBasket',
-  
-    methods: {
+        <div class="grand-total">Grand Total: US$ 22.30</div>
      
-    },
-   
-  }
-  </script>
-  
-  <style lang="scss">
-  
-  .basket {
-    padding: 60px 0;  
-    .items {
-      max-width: 800px;
-      margin: auto;
-      .item {
-        display: flex;
-        justify-content: space-between;
-        padding: 40px 0;
-        border-bottom: 1px solid lightgrey;
-        position: relative;
-  
-        .remove {
-          position: absolute;
-          top: 8px;
-          right: 0;
-          font-size: 11px;
-          text-decoration: underline;
+      </template>
+
+      <template v-else>
+        <h4>No items in bag yet</h4>
+      </template>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "ShoppingBasket",
+
+  methods: {},
+  computed: mapState(["productsInBag"]),
+};
+</script>
+
+<style lang="scss">
+.basket {
+  padding: 60px 0;
+  .items {
+    max-width: 800px;
+    margin: auto;
+    .item {
+      display: flex;
+      justify-content: space-between;
+      padding: 40px 0;
+      border-bottom: 1px solid lightgrey;
+      position: relative;
+
+      .remove {
+        position: absolute;
+        top: 8px;
+        right: 0;
+        font-size: 11px;
+        text-decoration: underline;
+        cursor: pointer;
+      }
+
+      .quantity-area {
+        margin: 8px auto;
+        height: 14px;
+
+        button {
+          width: 16px;
+          height: 16px;
+          display: inline-flex;
+          justify-content: center;
+          align-items: center;
           cursor: pointer;
         }
-  
-        .quantity-area {
-          margin: 8px auto;
-          height: 14px;
-  
-          button {
-            width: 16px;
-            height: 16px;
-            display: inline-flex;
-            justify-content: center;
-            align-items: center;
-            cursor: pointer;
-          }
-  
-          .quantity {
-  
-              margin: 0 4px;
-          }
-        }
-  
-        .photo {
-          img {
-            max-width: 80px;
-          }
-        }
-  
-        .description {
-          padding-left: 30px;
-          box-sizing: border-box;
-          max-width: 50%;
-  
-        }
-  
-        .price {
-          .amount {
-            font-size: 16px;
-            margin-left: 8px;
-            vertical-align: middle;
-  
-          }
+
+        .quantity {
+          margin: 0 4px;
         }
       }
-        .grand-total {
-            font-size: 24px;
-            font-weight: bold;
-            text-align: right;
-            margin-top: 8px;
+
+      .photo {
+        img {
+          max-width: 80px;
         }
-  
+      }
+
+      .description {
+        padding-left: 30px;
+        box-sizing: border-box;
+        max-width: 50%;
+      }
+
+      .price {
+        .amount {
+          font-size: 16px;
+          margin-left: 8px;
+          vertical-align: middle;
+        }
+      }
     }
-  
+    .grand-total {
+      font-size: 24px;
+      font-weight: bold;
+      text-align: right;
+      margin-top: 8px;
+    }
   }
-  
-  </style>
-  
+}
+</style>
